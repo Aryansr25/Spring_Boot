48d85147ed902177c45b5f11d7e62c9eecb1cfd4
Admin <Admin@DESKTOP-0AGM1IC.lan>
2026-03-30 19:11:49 +0530

    My first commit

---
+++ Personal_Tracker/src/test/java/com/tracker/Personal_Tracker/PersonalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.Personal_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
